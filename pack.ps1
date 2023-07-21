0f67ec9f84dc760c855f213ca2563a61f9468a53
personball <personball@163.com>
2023-07-21 15:07:40 +0800

    update csproj about nupkg metadata

--- pack.ps1
+++
@@ -1,5 +0,0 @@
-rm nupkg/*.nupkg
-rm Initializer/samples.zip
-7z a .\\Initializer\\samples.zip .\\Initializer\\Samples\\*
-dotnet pack -c Release
-
--- pack.sh
+++ pack.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+rm -rf .vz
+rm -rf .vzx
 rm nupkg/*.nupkg
 
 rm Initializer/samples.zip
